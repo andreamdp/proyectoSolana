344926a91c910d8f27eaf2462a5ac065931841c2
andreamdp <andremdq@gmail.com>
2013-02-21 23:49:20 -0300

    auth0.models

--- auth0/urls.py
+++ auth0/urls.py
@@ -3,15 +3,15 @@
 # This file is also used to provide a reliable view deployment for test purposes.
 
 from django.conf.urls.defaults import *
-
+#django.contrib.auth.
 urlpatterns = patterns('',
-    (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout'),
-    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
-    (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
-    (r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
-    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    (r'^login/$', 'auth0.views.login'),
+    (r'^logout/$', 'auth0.views.logout'),
+    (r'^password_change/$', 'auth0.views.password_change'),
+    (r'^password_change/done/$', 'auth0.views.password_change_done'),
+    (r'^password_reset/$', 'auth0.views.password_reset'),
+    (r'^password_reset/done/$', 'auth0.views.password_reset_done'),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'auth0.views.password_reset_confirm'),
+    (r'^reset/done/$', 'auth0.views.password_reset_complete'),
 )
 
