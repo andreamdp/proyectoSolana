d6044862e2edc9bf8fea477eb4c5ad9461331085
andreamdp <andremdq@gmail.com>
2013-02-25 17:42:56 -0300

    setting.py south adapter

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ DATABASES = {
     }
 }
 SOUTH_DATABASE_ADAPTERS = {
-    'default': "south.db.mysql"
+    'default': "south.db.postgresql"
 }
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
