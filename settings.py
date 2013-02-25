209fa9bd18ff7db2f9bff59ef8c772e2d8034394
andreamdp <andremdq@gmail.com>
2013-02-25 18:48:44 -0300

    prueba con mysql

--- settings.py
+++ settings.py
@@ -196,41 +196,6 @@ LOGGING = {
     }
 }
 
-import sys
-import urlparse
 
-# Register database schemes in URLs.
-urlparse.uses_netloc.append('mysql')
-
-# Parse database configuration from $DATABASE_URL
-
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-    
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
