a28e9940f936a6a3b2fb6c34a996a46ac2397849
andreamdp <andremdq@gmail.com>
2013-02-25 19:06:57 -0300

    prueba con mysql

--- settings.py
+++ settings.py
@@ -16,20 +16,20 @@ MENU_CACHE_TIME = -1
 import dj_database_url
 #
 #DATABASES['default'] =  dj_database_url.config()
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': '',                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'solanaABM012',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 
 	
-DATABASES = {'default': dj_database_url.config()} 
+#DATABASES = {'default': dj_database_url.config()} 
 #SOUTH_DATABASE_ADAPTERS = { 'default': "south.db.postgresql_psycopg2" } 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
