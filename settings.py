6bcc0300a459e5bc4d464d88fa38265ba745a396
andreamdp <andremdq@gmail.com>
2013-02-25 20:48:39 -0300

    postgres

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ import dj_database_url
 #}
 	
 DATABASES = {'default': dj_database_url.config()} 
-#SOUTH_DATABASE_ADAPTERS = { 'default': "south.db.postgresql_psycopg2" } 
+SOUTH_DATABASE_ADAPTERS = { 'default': "south.db.postgresql_psycopg2" } 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
