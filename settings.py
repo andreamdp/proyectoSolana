f606cfd1b544d98d274bea9163ce6e35a5ad73f3
andreamdp <andremdq@gmail.com>
2013-02-25 21:04:56 -0300

    postgres

--- settings.py
+++ settings.py
@@ -159,7 +159,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'south',
+  #  'south',
 )
 
 GRAPPELLI_ADMIN_TITLE = 'Solana SRL'
